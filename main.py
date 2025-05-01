7ce875a97f9c9e06c53c10078bc171d220a3cd8f
sphisi <143594972+sphisi@users.noreply.github.com>
2025-05-01 22:18:34 +0000

    just the write

--- main.py
+++ main.py
@@ -2,41 +2,61 @@
 #login list
 logins = []
 
+
 #if for 1
 def loginn():
-    contiune_username = 1
-    login_username = input("username: ")
     #opens file to check if username and password are in
-    with open("logins.csv") as file:
-        print(login_username)
-        for line in file:
-            if login_username in line:
-                username, password = line.strip().split(",")
-                password = password
-                print(password)
-                #checks if password is in the value corrosponding the the username
-                login_password = input("Password: ").strip.strip()
-                print(login_password)
-                for line in file:
-                    if login_password in line:
-                        #asks if to change password or sxt
-                        login_options = input("change password: 1\nLogout: 2\nChoice: ")
-                        #if change password opens files as read
-                        if login_options == "1":
-                                #creates new line to append file execpt for changeding line
-                                for line in file:
-                                    templist = []
-                                new_password = input("New password: ")
-                                
-                                #opens files as write and writes templist to file
-                        with open("logins", "w") as file:
-                            file.write(templist)
-                    else:
-                        exit
+    username_check()
+    #checks if password is in the value corrosponding the the username
+    password_check()
+    #asks if to change password or sxt
+
+def username_check():
+    contiune_username = 1
+    while contiune_username == 1:
+        global login_username
+        login_username = input("username: ")
+        with open("logins.csv") as file:
+            # print(login_username)
+            for line in file:
+                if login_username in line:
+                    global password
+                    username, password = line.strip().split(",")
+                    contiune_username = 2
+                else:
+                    print("username not found")
+
+def password_check():
+    contiune_password = 1
+    while contiune_password == 1:
+        login_password = input("Password: ")
+        # print(login_password)
+        with open("logins.csv") as file:
+            print(login_password)
+            for line in file:
+                if login_password in line:
+                    post_login_options()
+                    contiune_password = 2
                 else:
                     print("invalid password")
-        else:
-            print("username not found")
+
+def post_login_options():
+    login_options = input("change password: 1\nLogout: 2\nChoice: ")
+    #if change password opens files as read
+    if login_options == "1":
+        #creates new line to append file execpt for changeding line
+        with open("logins.csv") as file:
+            templist = []
+            new_password = input("New password: ")
+            for line in file:
+                templist.append(line)
+                #opens files as write and writes templist to file
+                with open("logins", "w") as file:
+                    file.write(templist)
+                    exit()
+    else:
+        exit
+
 
 
 #if for register 2
