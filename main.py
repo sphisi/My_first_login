e03a35b88686faf3a5868e968b528ac4e23cb797
sphisi <143594972+sphisi@users.noreply.github.com>
2025-05-01 05:04:29 +0000

    finishing

--- main.py
+++ main.py
@@ -1,75 +1,75 @@
+
+#login list
+logins = []
+
 #if for 1
-def opt1():
+def loginn():
+    contiune_username = 1
     login_username = input("username: ")
     #opens file to check if username and password are in
     with open("logins.csv") as file:
         print(login_username)
         for line in file:
             if login_username in line:
-                listusername = []
-                listpassword = []
-                login_password = input("Whats your password: ")
-                username, password = line.rstrip().split(",")
-                password = password.strip()
-                # print(f"{username},{password}")
-                #get the index of the username
-                index = login_password[1]
-                #checks if password is in the value corrosponding the the username, dosent work :( properly
-                for line in index:
-                    if password in loginsexit:
+                username, password = line.strip().split(",")
+                password = password
+                print(password)
+                #checks if password is in the value corrosponding the the username
+                login_password = input("Password: ").strip.strip()
+                print(login_password)
+                for line in file:
+                    if login_password in line:
                         #asks if to change password or sxt
                         login_options = input("change password: 1\nLogout: 2\nChoice: ")
                         #if change password opens files as read
                         if login_options == "1":
-                            with open("logins.csv") as file:
                                 #creates new line to append file execpt for changeding line
                                 for line in file:
                                     templist = []
                                 new_password = input("New password: ")
                                 
                                 #opens files as write and writes templist to file
-                            with open("logins", "w") as file:
-                                file.write(templist)
-                        else:
-                            exit
+                        with open("logins", "w") as file:
+                            file.write(templist)
                     else:
-                        print("invalid password")
-            else:
-                print("username not found")
+                        exit
+                else:
+                    print("invalid password")
+        else:
+            print("username not found")
 
 
 #if for register 2
-def opt2():
+def registerr():
+    contine_register = 1
     register_username = input("new username: ")
-    register_password = input("New password: ")
-    if len(register_password) < 4:
-        print("to small")
-    else:
-        logins.append(f"{register_username},{register_password}")
-    #add usernames to file
-    with open("logins.csv", "a") as file:
-        loginsexit.append(f"{register_username},{register_password}")
-        file.write(f"\n{register_username},{register_password}")
+    while contine_register == 1:
+        register_password = input("New password: ")
+        if len(register_password) < 4:
+            print("to small")
+        else:
+            logins.append(f"{register_username},{register_password}")
+            contine_register = 2
+        #add usernames to file
+            with open("logins.csv", "a") as file:
+                file.write(f"\n{register_username},{register_password}")
 
-
-#login list
-logins = []
 
 #var for contiune loop
 continuecode = 1
 def contorexit():
     continuecode = 2
     return continuecode
-#menu
 
+#menu
 while continuecode == 1:
     menu_options = input("login: 1\nregister: 2\nexit: 3\nChoice: ")
     if menu_options == "1":
-        opt1()
+        loginn()
     elif menu_options == "2":
-        opt2()
+        registerr()
     elif menu_options == "3":
-        sys.exit
+        exit()
     else:
         print("pick a valid option")
     contorexit()
